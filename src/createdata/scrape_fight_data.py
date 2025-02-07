2596c0f8dadc334f74bf2e9564a02ec264414801
Patrick Dornian <patrick.dornian@gmail.com>
2025-02-06 17:58:01 -0700

    refactored fight link scraping. variable name semantics.

--- src/createdata/scrape_fight_data.py
+++ src/createdata/scrape_fight_data.py
@@ -9,8 +9,8 @@ from src.createdata.scrape_fight_links import UFCLinks
 from src.createdata.utils import make_soup, print_progress
 
 from src.createdata.data_files_path import (  # isort:skip
-    NEW_EVENT_AND_FIGHTS,
-    TOTAL_EVENT_AND_FIGHTS,
+    NEW_FIGHTS_DATA_PATH,
+    TOTAL_FIGHTS_DATA_PATH,
 )
 
 
@@ -24,71 +24,66 @@ class FightDataScraper:
 ;win_by;last_round;last_round_time;Format;Referee;date;location;Fight_type;Winner\n"
         )
 
-        self.NEW_EVENT_AND_FIGHTS_PATH = NEW_EVENT_AND_FIGHTS
-        self.TOTAL_EVENT_AND_FIGHTS_PATH = TOTAL_EVENT_AND_FIGHTS
+        self.NEW_FIGHTS_DATA_PATH = NEW_FIGHTS_DATA_PATH
+        self.TOTAL_FIGHTS_DATA_PATH = TOTAL_FIGHTS_DATA_PATH
 
     def create_fight_data_csv(self) -> None:
         print("Scraping links!")
 
         ufc_links = UFCLinks()
-        new_events_and_fight_links, all_events_and_fight_links = (
-            ufc_links.get_event_and_fight_links()
-        )
-        print("Successfully scraped and saved event and fight links!\n")
-        print("Now, scraping event and fight data!\n")
+        new_fight_links, all_fight_links = ufc_links.get_fight_links()
+        print("Successfully scraped and saved fight links!\n")
+        print("Now, scraping fight data!\n")
 
-        if not new_events_and_fight_links:
-            # if no new events
-            if self.TOTAL_EVENT_AND_FIGHTS_PATH.exists():
-                # if csv previously generated, load from data csv
+        # are there new fight links to scrap data from?
+        if not new_fight_links:
+            # if there's no new fight links
+            if self.TOTAL_FIGHTS_DATA_PATH.exists():
+                # if fight data csv file exists.
+
+                # assume fight data up to date
+                # this is not actually necessarily true
+                # but good enough for now
                 print(
                     f"""No new fight data to scrape.
                         {self.TOTAL_EVENT_AND_FIGHTS_PATH} up to date."""
                 )
-                return
+                return None
             else:
-                # if no csv, scrape everything and make it.
+                # if no data csv, scrape all fights and make it.
                 self._scrape_raw_fight_data(
-                    all_events_and_fight_links,
-                    filepath=self.TOTAL_EVENT_AND_FIGHTS_PATH,
+                    all_fight_links,
+                    filepath=self.TOTAL_FIGHTS_PATH,
                 )
         else:
-            # scrape new event data
+            # scrape only fights from new events
             self._scrape_raw_fight_data(
-                new_events_and_fight_links, filepath=self.NEW_EVENT_AND_FIGHTS_PATH
+                new_fight_links, filepath=self.NEW_EVENT_AND_FIGHTS_PATH
             )
 
-            new_event_and_fights_data = pd.read_csv(self.NEW_EVENT_AND_FIGHTS_PATH)
-            old_event_and_fights_data = pd.read_csv(self.TOTAL_EVENT_AND_FIGHTS_PATH)
+            new_fights_data = pd.read_csv(self.NEW_FIGHTS_PATH)
+            old_fights_data = pd.read_csv(self.TOTAL_FIGHTS_PATH)
 
             # verify same column count
-            assert len(new_event_and_fights_data.columns) == len(
-                old_event_and_fights_data.columns
-            )
+            assert len(new_fights_data.columns) == len(old_fights_data.columns)
 
             # restricts new event cols to those with labels of old events/ensures same col order
-            # feels like mergine new/old fight data should be a seperate method
-            new_event_and_fights_data = new_event_and_fights_data[
-                list(old_event_and_fights_data.columns)
-            ]
+            # feels like merging new/old fight data should be a seperate method
+            new_fights_data = new_fights_data[list(old_fights_data.columns)]
 
             # might be worth verifying integrity here
             latest_total_fight_data = pd.concat(
-                [new_event_and_fights_data, old_event_and_fights_data],
+                [new_fights_data, old_fights_data],
                 axis=1,
                 ignore_index=True,
             )
 
-            # latest_total_fight_data = new_event_and_fights_data.append(
-            #     old_event_and_fights_data, ignore_index=True
-            # )
+            latest_total_fight_data.to_csv(self.TOTAL_FIGHTS_DATA_PATH, index=None)
+            print(f"Updated {self.TOTAL_FIGHTS_DATA_PATH} with new fight data")
+            os.remove(self.NEW_EVENT_AND_FIGHTS_DATA_PATH)
+            print("Removed temporary files.")
 
-            latest_total_fight_data.to_csv(self.TOTAL_EVENT_AND_FIGHTS_PATH, index=None)
-            print(f"Updated {self.TOTAL_EVENT_AND_FIGHTS_PATH} with new fight data")
-            os.remove(self.NEW_EVENT_AND_FIGHTS_PATH)
-            print("Removed temproary files.")
-
-        print("Successfully scraped and saved ufc fight data!\n")
+        print("Successfully scraped and saved UFC fight data!")
 
     def _scrape_raw_fight_data(
         self, event_and_fight_links: Dict[str, List[str]], filepath
@@ -117,19 +112,14 @@ class FightDataScraper:
 
         return total_fight_stats
 
-    # unsure why any of this stuff is class methods.
-    # seems like it could be instance defined.
-
-    def _get_total_fight_stats(
-        self, event_and_fight_links: Dict[str, List[str]]
-    ) -> str:
+    def _get_total_fight_stats(self, fight_links: Dict[str, List[str]]) -> str:
         total_stats = ""
 
-        fight_count = len(event_and_fight_links)
+        fight_count = len(fight_links)
         print(f"Scraping data for {fight_count} fights: ")
         print_progress(0, fight_count, prefix="Progress:", suffix="Complete")
 
-        for index, (event, fights) in enumerate(event_and_fight_links.items()):
+        for index, (event, fights) in enumerate(fight_links.items()):
             event_soup = make_soup(event)
             event_info = self._get_event_info(event_soup)
 
@@ -140,18 +130,17 @@ class FightDataScraper:
                     futures.append(
                         executor.submit(
                             self._get_fight_stats_task,
-                            # self=self,
                             fight=fight,
                             event_info=event_info,
                         )
                     )
                 for future in concurrent.futures.as_completed(futures):
-                    fighter_stats = future.result()
-                    if fighter_stats != "":
+                    fight_stats = future.result()
+                    if fight_stats != "":
                         if total_stats == "":
-                            total_stats = fighter_stats
+                            total_stats = fight_stats
                         else:
-                            total_stats = total_stats + "\n" + fighter_stats
+                            total_stats = total_stats + "\n" + fight_stats
                     print_progress(
                         index + 1, fight_count, prefix="Progress:", suffix="Complete"
                     )
@@ -181,6 +170,7 @@ class FightDataScraper:
                 .replace(" ,", ",")
             )
 
+        # hardcoded here to ignore first 3 cols of significant strikes table
         fight_stats[1] = ";".join(fight_stats[1].split(",")[6:])
         fight_stats[0] = ";".join(fight_stats[0].split(","))
         fight_stats = ";".join(fight_stats)
