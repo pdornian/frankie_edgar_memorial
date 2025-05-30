c3ac016cc715ffcc98ea4d0e6bba3b6dc2d6fe62
Patrick Dornian <patrick.dornian@gmail.com>
2025-05-30 11:26:27 -0600

    commented out defunct stuff from master create data script

--- create_ufc_data.py
+++ create_ufc_data.py
@@ -1,24 +1,22 @@
-#moved this to root, might make it explode, fix later.
-
 import time
-from src.ufctools.legacy.preprocess import Preprocessor
+# from src.ufctools.legacy.preprocess import Preprocessor
 from src.ufctools.legacy.scrape_fight_data import FightDataScraper
-from src.ufctools.legacy.scrape_fighter_details import FighterDetailsScraper
+# from src.ufctools.legacy.scrape_fighter_details import FighterDetailsScraper
 
 time_start = time.time()
 print("Creating fight data \n")
 fight_data_scraper = FightDataScraper()
-fight_data_scraper.create_fight_data_csv()  # Scrapes raw ufc fight data from website
+fight_data_scraper.scrape_new_fights()  # Scrapes raw ufc fight data from website
 print(f'elapsed seconds = {(time.time() - time_start):.2f}')
 
-time_start = time.time()
-print("Creating fighter data \n")
-fighter_details_scraper = FighterDetailsScraper()
-fighter_details_scraper.create_fighter_data_csv()  # Scrapes raw ufc fighter data from website
-print(f'elapsed seconds = {(time.time() - time_start):.2f}')
+# time_start = time.time()
+# print("Creating fighter data \n")
+# fighter_details_scraper = FighterDetailsScraper()
+# fighter_details_scraper.create_fighter_data_csv()  # Scrapes raw ufc fighter data from website
+# print(f'elapsed seconds = {(time.time() - time_start):.2f}')
 
-time_start = time.time()
-print("Starting Preprocessing \n")
-preprocessor = Preprocessor()
-preprocessor.process_raw_data()  # Preprocesses the raw data and saves the csv files in data folder
-print(f'elapsed seconds = {(time.time() - time_start):.3f}')
+# time_start = time.time()
+# print("Starting Preprocessing \n")
+# preprocessor = Preprocessor()
+# preprocessor.process_raw_data()  # Preprocesses the raw data and saves the csv files in data folder
+# print(f'elapsed seconds = {(time.time() - time_start):.3f}')
